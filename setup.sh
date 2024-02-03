4f9bc78f5006eac6ee8c35a44d7853a96828c123
stromokocur5 <gabrielkohut3@proton.me>
2024-02-03 14:46:12 +0100

    uh

---
+++ setup.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/sh
+
+# Install rpm-fusion repos and install codecs
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf config-manager --enable fedora-cisco-openh264
+sudo dnf swap ffmpeg-free ffmpeg --allowerasing
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf groupupdate sound-and-video
+sudo dnf install intel-media-driver
+sudo dnf install libva-intel-driver
+sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld
+sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
+sudo dnf install nvidia-vaapi-driver
+sudo dnf install rpmfusion-nonfree-release-tainted
+sudo dnf --repo=rpmfusion-nonfree-tainted install "*-firmware" 
+
+# Install docker
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl enable --now docker
+
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Install deno
+curl -fsSL https://deno.land/install.sh | sh
+
+# Install bun
+curl -fsSL https://bun.sh/install | bash
+
+# Install tauri
+cargo install create-tauri-app --locked
+
+# Install tailwind
+curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/download/v3.4.1/tailwindcss-linux-x64
+chmod +x tailwindcss-linux-x64
+test -d $HOME/.local/bin || mkdir $HOME/.local/bin
+sudo mv tailwindcss-linux-x64 $HOME/.local/bin/tailwindcss
+
+# Install other programs
+sudo dnf install -y just rsync git go node ansible helix firefox pandoc feh mpv inkscape gimp krita blender kdenlive flatpak
+
+# Add flathub to flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# Install OpenTofu
+cat >/etc/yum.repos.d/opentofu.repo <<EOF
+[opentofu]
+name=opentofu
+baseurl=https://packages.opentofu.org/opentofu/tofu/rpm_any/rpm_any/\$basearch
+repo_gpgcheck=0
+gpgcheck=1
+enabled=1
+gpgkey=https://get.opentofu.org/opentofu.gpg
+       https://packages.opentofu.org/opentofu/tofu/gpgkey
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+metadata_expire=300
+
+[opentofu-source]
+name=opentofu-source
+baseurl=https://packages.opentofu.org/opentofu/tofu/rpm_any/rpm_any/SRPMS
+repo_gpgcheck=0
+gpgcheck=1
+enabled=1
+gpgkey=https://get.opentofu.org/opentofu.gpg
+       https://packages.opentofu.org/opentofu/tofu/gpgkey
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+metadata_expire=300
+EOF
+sudo dnf install -y tofu
+
+# Clone configs
+git clone https://github.com/stromokocur5/helix ~/.config/helix
+git clone https://github.com/stromokocur5/dwm ~/.config/dwm
+git clone https://github.com/stromokocur5/st ~/.config/st
+git clone https://github.com/stromokocur5/tmux ~/.config/tmux
